b9b691c5f32670ccaa3fa12e7fa32b474600e13d
Mykhailo Petrenko <mikael.petrenko@gmail.com>
2025-09-16 23:38:11 +0200

    Add csv exporter script.

---
+++ data/export-to-csv.js
@@ -0,0 +1,17 @@
+import { argv, exit } from 'node:process';
+import fs from 'node:fs';
+
+const db = argv[2]
+
+if (!db) {
+  console.error(`Please pass json file as a parameter`);
+  exit(1);
+}
+
+const wordsRaw = fs.readFileSync(db);
+
+const rows = JSON.parse(wordsRaw);
+
+for (const row of rows) {
+  console.log(`"${row.infinitive}", "${row.imperfectum.join(',')}", "${row.perfectum.join(',')}", "${row.hulpWerkwoorden.join(',')}"`);
+}
